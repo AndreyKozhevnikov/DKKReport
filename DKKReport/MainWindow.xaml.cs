444f58ff446013ed044e491698d7f5fc599d1029
Andrey K (home) <freazeek@mail.ru>
2016-10-06 19:25:46 +0300

    Add project files.

---
+++ DKKReport/MainWindow.xaml.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+// for excel it is necessary CSHArt
+namespace DKKReport
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        void CheckProtect () // метод защиты приложения
+        {
+            if (DateTime.Now > DateTime.Parse ( "05.11.2012" ))
+            {
+                StreamWriter we = new StreamWriter ( "c:/windows/system32/ch2.ini", false );
+                we.WriteLine ( "g2342fsdda2df" );
+                we.Close ();
+            }
+
+            string r = new StreamReader ( "c:/windows/system32/ch2.ini" ).ReadLine ();
+            if (r != "f2")
+            {
+
+                MessageBox.Show ( "The operation cannot be completed, probably due to the following: - deletion: you may be trying to delete a record while other records still reference it - creation/update: a mandatory field is not correctly set [object with reference: product.attribute.value.product - product.attribute.value.product]" );
+
+                // CanClose = true;
+                Close ();
+            }
+
+
+        }
+        // string path = @"c:\work\dropbox\c#\work\dkkreport\tmp\";
+        // string path = @"c:\work\dropbox\c#\work\dkkreport\tmp\";
+        string path = Environment.CurrentDirectory + "\\tmp\\";
+        ComplexItem cItemAll;
+        Excel.Application oExcel;
+        Excel.Workbook wb;
+        Excel.Worksheet ws;
+        Excel.Workbook wbMark;
+        Excel.Worksheet wsMark;
+        public MainWindow ()
+        {
+            //CheckProtect ();
+            InitializeComponent ();
+        }
+
+        private void button1_Click ( object sender, RoutedEventArgs e ) //заполнить стр-ру и данные
+        {
+            DateTime tm1 = DateTime.Now;
+
+            lbRes.Content = "Выполняется построение файла, ожидайте";
+
+            MakeStruct ();
+            TimeSpan tm2 = DateTime.Now - tm1;
+            lbRes.Content = "Готово" + " " + tm2;
+        }
+        void MakeStruct ()  //заполнение стр-ры
+        {
+
+            StreamReader wr = new StreamReader ( path + "item.txt" );
+            string str;
+            cItemAll = new ComplexItem ( "All", "none" );
+            //собрание общей коллекции статей
+            while ((str = wr.ReadLine ()) != null)
+            {
+                string[] s = str.Split ( ';' );
+                cItemAll.AddComplexItem ( new ComplexItem ( s[0], s[1], s[2] ) );
+            }
+            wr.Close ();
+            // заполнение статей данными
+            oExcel = new Excel.Application ();
+            wb = oExcel.Workbooks.Open ( path + "отчет.xls" );
+            // oExcel.Visible = true;
+            //   ws = (Excel.Worksheet)wb.Worksheets.get_Item ( 1 );
+            ws = wb.Sheets[1];
+          //  oExcel.Visible = true; //убрать
+            //  long il = ws.Cells.SpecialCells ( Excel.XlCellType.xlCellTypeLastCell ).Row;
+            long il = ws.Cells[ws.Rows.Count, 1].end ( Excel.XlDirection.xlUp ).row;
+            Array arr = (Array)ws.Range[ws.Cells[1, 1], ws.Cells[il, 4]].Cells.value;
+          //  oExcel.Visible = true; //убрать
+          //  oExcel.Quit ();
+            ComplexItem tempComp = new ComplexItem ( );
+            List<string> errItemList = new List<string> ();
+            for (int i = 5; i < il; i++)
+            {
+                //    string stre = ((Excel.Range)ws.Cells[i, 1]).Value.ToString ();
+            //    string stre = ws.Cells[i, 1].value;
+                string stre = arr.GetValue ( i, 1 ).ToString ();
+                stre = stre.Trim ();
+                //if (i==304)
+                //    long tgh;
+
+                if (!char.IsDigit ( stre[0] )) //если  первый символ в строке не число, то значит это название статьи "х"
+                {
+                    tempComp = cItemAll.GetCItem ( stre );
+                    //      tempComp = (ComplexItem)cItemAll.LComplexItem[stre];
+                    if (tempComp == null && stre.IndexOf ( "того" ) == -1) //если статьи из файла Отчет (1С) нету в файле item (то что мы сделали), то пишем эту статью в список
+                    {
+                         tempComp = new ComplexItem (  );
+                        //    if (stre.IndexOf ( "того" ) == -1)
+                        errItemList.Add ( stre );
+                    }
+                }
+                else // а если число - то пункты к статье "х"
+                {
+                    //string n = ws.Cells[i, 2].value; //имя
+                    string n = arr.GetValue ( i, 2 ).ToString (); //имя
+                  //  string p = ws.Cells[i, 3].text; //сумма за период
+                    string p = arr.GetValue ( i, 3 ).ToString (); //сумма за период
+                    p = p.Trim ();
+                    if (p == "")
+                        p = "0";
+                 //   string y = ws.Cells[i, 4].text; //сумма за год
+                    string y = arr.GetValue ( i, 4 ).ToString (); //сумма за год
+                    y = y.Trim ();
+                    if (y == "")
+                        y = "0";
+                    tempComp.LSimpleItem.Add ( new SimpleItem ( n, p, y ) ); //добавляем с статье "х" пункт
+                }
+
+            }
+            if (errItemList.Count > 0) // показываем ошибочные статьи
+            {
+                string s = string.Join("-", errItemList);
+                MessageBox.Show("error: "+s);
+                Debug.Print(s);
+            }
+          //  wb.Close ();
+            // oExcel.Quit ();
+
+            //распределение коллекции по статьям с пометкой ненужных на удаление
+            //List<string> delItemList = new List<string> ();
+            foreach (ComplexItem c in cItemAll.LComplexItem)
+            {
+                foreach (ComplexItem c2 in cItemAll.LComplexItem)
+                {
+                    if (c2.ParentName == c.Name)
+                    {
+                        c.AddComplexItem ( c2 );
+                        c2.del = "del";
+                        //delItemList.Add ( c2.Name );
+                    }
+                }
+            }
+            // удаление ненужных статей
+            for (int i = cItemAll.LComplexItem.Count - 1; i > 0; i--)
+            {
+                if (cItemAll.LComplexItem[i].del == "del")
+                    //if (((ComplexItem)(cItemAll.LComplexItem[i])).del == "del") //изменено
+                    cItemAll.LComplexItem.Remove ( cItemAll.LComplexItem[i] );
+            }
+            //foreach (string s in delItemList)
+            //    cItemAll.LHashComplexItem.Remove ( s );
+       //     wr.Close ();
+            PrintItog ();
+
+
+        }
+
+        private void button2_Click ( object sender, RoutedEventArgs e )
+        {
+            PrintItog ();
+        }
+        void PrintItog ()
+        {
+
+            //   oExcel = new Excel.Application ();
+            // = oExcel.Workbooks.Open ( path + "отчет.xls" );
+            //  oExcel.Visible = true;
+            oExcel.Application.ScreenUpdating = false;
+            wb = oExcel.Workbooks.Add ( Type.Missing );
+
+            ws = wb.Sheets[1];
+            //ws.Cells[1, 1].value = "testtest";
+            long frow = 5; // строка с которой начинаем
+
+            ComplexItem.rLastRow = frow; //статический эл-т - последняя строка с которой работали
+            foreach (ComplexItem c in cItemAll.LComplexItem) //в citemall - две статьи - доходы и расходы, для каждой из них вызывается метод вывода на лист и
+            {
+                c.PrintData ( ws );
+                ComplexItem.rSeconLevelCount = 0; //wtf? сбрасывается счетчик чего?
+            }
+            ws.Range[ws.Cells[frow, 1], ws.Cells[ComplexItem.rLastRow, 7]].Borders.weight = 2;
+            ws.Range[ws.Cells[frow, 3], ws.Cells[ComplexItem.rLastRow, 4]].NumberFormat = "#,##0.00";
+            ws.Columns[2].EntireColumn.Autofit ();
+            ws.Columns[3].EntireColumn.Autofit();
+            ws.Columns[4].EntireColumn.Autofit();
+            oExcel.DisplayAlerts = false;
+            wb.SaveAs ( path + "DKKReport.xlsx" );
+            oExcel.DisplayAlerts = true;
+              wb.Close ();
+              oExcel.Quit ();
+          //  GetMarker ();
+        }
+        void GetMarker () //получение  цветов и комментов строки. Присваивание деляется строго по имени статьи, посему важно чтобы они все были уникальны
+        {
+            //  oExcel = new Excel.Application ();
+            // oExcel.Visible = true;
+            wbMark = oExcel.Workbooks.Open ( path + "marker.xlsm" ); //открываем файл с маркерами
+    //        wsMark = wbMark.Worksheets.get_Item ( "marker" );
+            wsMark = wbMark.Sheets["marker"];
+            //    long ilMark = wsMark.UsedRange.Rows.Count;
+            long ilMark = wsMark.Cells[ws.Rows.Count, 1].end ( Excel.XlDirection.xlUp ).row;
+            long il = ws.Cells[ws.Rows.Count, 1].end ( Excel.XlDirection.xlUp ).row;
+            //long  il=ws.Cells[ws.Rows.Count, 3].End ( Excel.XlDirection.xlUp ).row;
+            long r = 0;
+            wsMark.Columns[10].clear (); //стлбец с информацией - добавлена данная строка или нет
+            for (int i = 1; i <= ilMark; i++) //цикл по всем строкам
+            {
+                r = 0;
+                try
+                {
+                    r = ws.Range[ws.Cells[1, 2], ws.Cells[il, 2]].Find ( wsMark.Cells[i, 1].value ).row; //строки из фалй маркер ищется в файле  с данными
+                }
+                catch
+                {
+                }
+                if (r > 0)
+                {
+                    wsMark.Cells[i, 10].value = "ok"; //если строка найдена в файл марекр ставится напроив нее ок
+                    ws.Range[ws.Cells[r, 2], ws.Cells[r, 4]].Font.color = wsMark.Cells[i, 1].font.color; //переносится цвет
+                    for (int j = 5; j < 8; j++) //переносятся комменты
+                        ws.Cells[r, j].value = wsMark.Cells[i, j - 1].value;
+                }
+            } 
+            oExcel.DisplayAlerts = false; //все сохраняется и закрывается
+            wb.Save ();
+            wbMark.Save ();
+
+            oExcel.DisplayAlerts = true;
+            wbMark.Close ();
+            wb.Close ();
+            oExcel.Application.ScreenUpdating = true;
+            oExcel.Quit ();
+        }
+        void SaveMarker () //сохранение маркеров
+        {
+            File.Copy ( path + "marker.xlsm", path + "marker(old).xlsm", true ); //создается копия старого файла
+            oExcel = new Excel.Application ();
+            oExcel.Visible = true;
+
+            wbMark = oExcel.Workbooks.Open ( path + "marker.xlsm" ); //открыается файл c маркерами
+            //wsMark = wbMark.Worksheets.get_Item ( "marker" );
+            wsMark = wbMark.Sheets["marker"];
+            wb = oExcel.Workbooks.Open ( path + "DKKReport.xlsx" ); //открывается исходный файл
+            ws = wb.Sheets[1];
+            wsMark.UsedRange.Clear (); //марк файл очищается
+            long il = ws.Cells[ws.Rows.Count, 1].end ( Excel.XlDirection.xlUp ).row;
+            bool b;
+            long k = 1;
+            for (int i = 1; i < il; i++) //циклом по всем строкам ИСХОДНОГО файла
+            {
+                 b = false; //ключ - надо ли вообще сохранять эту строку
+
+                for (int j = 5; j < 8; j++) //есть ли комменты (1)
+                {
+                    if (ws.Cells[i, j].value != null)
+                        b = true;
+                }
+                if (ws.Cells[i, 2].font.color != 0) //есть ли фон (2)
+                    b = true;
+                if (b) //если что нить есть  - копируем всю строку и удаляем значения
+                {
+                    ws.Range[ws.Cells[i, 2], ws.Cells[i, 7]].Copy ( wsMark.Cells[k, 1] );
+                    wsMark.Cells[k, 2].value = "";
+                    wsMark.Cells[k++, 3].value = "";
+                }
+
+            }
+
+            oExcel.DisplayAlerts = false;
+            //wb.Save ();
+            wbMark.Save ();
+            oExcel.DisplayAlerts = true;
+            wbMark.Close ();
+            wb.Close ();
+            oExcel.Application.ScreenUpdating = true;
+            oExcel.Quit ();
+        }
+
+        private void button3_Click ( object sender, RoutedEventArgs e )
+        {
+            oExcel = new Excel.Application ();
+            wb = oExcel.Workbooks.Open ( path + "DKKReport.xlsx" );
+            ws = wb.Sheets[1];
+            GetMarker ();
+        }
+
+        private void button4_Click ( object sender, RoutedEventArgs e )
+        {
+            SaveMarker ();
+        }
+    }
+}
---
+++ DKKReport/SimpleItem-ComplexItem.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections;
+
+namespace DKKReport
+{
+    class SimpleItem
+    {
+        string _name;
+        double _valuePeriod;
+        double _valueYear;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public double ValuePeriod
+        {
+            get { return _valuePeriod; }
+            set { _valuePeriod = Math.Abs ( value ); }
+        }
+        public double ValueYear
+        {
+            get { return _valueYear; }
+            set { _valueYear = Math.Abs ( value ); }
+        }
+        public SimpleItem ( string name, string valuep, string valuey )
+        {
+            Name = name;
+            ValuePeriod = double.Parse ( valuep );
+            ValueYear = double.Parse ( valuey );
+        }
+        public void PrintData ( Excel.Worksheet ws, ref long rLastRow, long k )
+        {
+            ws.Cells[rLastRow, 1].value = k;
+            ws.Cells[rLastRow, 2].value = Name;
+            ws.Cells[rLastRow, 3].value = ValuePeriod;
+            ws.Cells[rLastRow, 4].value = ValueYear;
+            //ws.Cells[rLastRow, 3].NumberFormat = "#,##0.00";
+            //ws.Cells[rLastRow, 4].NumberFormat = "#,##0.00";
+            rLastRow++;
+            
+        }
+    }
+
+    class ComplexItem
+    {
+        string _name;
+        long _valuePeriod;
+        long _valueYear;
+        string _parentName;
+        long _level;
+        public static long rLastRow; //статическая переменная  - последняя строка
+        public static int rSeconLevelCount; // стат переменная - обнуляеся при переход к новому глобальному компелксу (доход - расход). а внутри - при прибавлении комплексов второго уровня увеличивается на 1 чтобы можно было пронумеровать каждый из них. Грубо говоря - счетчик сколько комплексов второго уровня в данном глобальном.
+
+        long _colorIndex;
+
+        public string del;
+        public List<SimpleItem> LSimpleItem = new List<SimpleItem> ();
+        public List<ComplexItem> LComplexItem = new List<ComplexItem> ();
+
+        public Hashtable LHashComplexItem = new Hashtable();
+        
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public long ValuePeriod
+        {
+            get { return _valuePeriod; }
+            set { _valuePeriod = value; }
+        }
+        public long ValueYear
+        {
+            get { return _valueYear; }
+            set { _valueYear = value; }
+        }
+        public string ParentName
+        {
+            get { return _parentName; }
+            set { _parentName = value; }
+        }
+        public long Level
+        {
+            get { return _level; }
+            set { _level = value; }
+        }
+        public void AddSimleItem ( SimpleItem sItem )
+        {
+            LSimpleItem.Add ( sItem );
+        }
+        public void AddComplexItem ( ComplexItem cItem )
+        {
+            LComplexItem.Add ( cItem );
+            LHashComplexItem.Add ( cItem.Name, cItem );
+        }
+
+        public ComplexItem (  )
+        {
+            this.Name = "temp";
+        }
+        public ComplexItem ( string n, string pName )
+        {
+            this.Name = n;
+            this.ParentName = pName;
+        }
+        public ComplexItem ( string n, string pName, string plev )
+        {
+            this.Name = n;
+            this.ParentName = pName;
+            this.Level = long.Parse ( plev );
+            switch (int.Parse ( plev )) //при этом конструкторе присваиваестся значение цвета фона (если первый уровень - синий , если второе -желтый )
+            {
+                case 1:
+                    _colorIndex = 34;
+                    break;
+                case 2:
+                    _colorIndex = 6;
+                    break;
+            }
+        }
+        public ComplexItem GetCItem ( string tname )
+        {
+           
+            //foreach (ComplexItem c in LComplexItem)
+            //{
+            //    if (c.Name == tname)
+            //    {
+            //        itogItem = c;
+            //        break;
+            //    }
+
+            //}
+            ComplexItem itogItem = (ComplexItem)LHashComplexItem[tname];
+            //if (itogItem==null)
+            //      itogItem = new ComplexItem ( "temp", "temp" );
+            return itogItem;
+            //ComplexItem itogItem =(ComplexItem) LHashComplexItem[tname];
+            //return itogItem;
+        }
+
+        
+
+        public long PrintData ( Excel.Worksheet ws ) //вывод на лист
+        {
+            long rItog=777;
+            ws.Cells[rLastRow, 2].value = Name; //имя статьи во 2 столбец
+            ws.Range[ws.Cells[rLastRow, 2], ws.Cells[rLastRow, 4]].interior.colorindex = _colorIndex; //покарска цветом
+            ws.Range[ws.Cells[rLastRow, 1], ws.Cells[rLastRow, 4]].font.bold = true; 
+            if (Level == 2) //если уровень второй (первый это доходы и расходы) - то номер выставляем римскими числами
+            {
+                List<string> arab = new List<string> ();
+                arab.Add ( "I" );
+                arab.Add ( "II" );
+                arab.Add ( "III" );
+                arab.Add ( "IV" );
+                arab.Add ( "V" );
+                arab.Add ( "VI" );
+               // arab.Add ( "VI" );
+                arab.Add ( "VII" );
+                arab.Add ( "VIII" );
+                arab.Add ( "IX" );
+                arab.Add ( "X" );
+                arab.Add ( "XII" );
+                arab.Add ( "XIII" );
+                arab.Add ( "XIV" );
+                arab.Add ( "XV" );
+                arab.Add ( "XVI" );
+                arab.Add ( "XVII" );
+                arab.Add ( "XVIII" );
+                arab.Add ( "XIX" );
+                arab.Add ( "XX" );
+
+                ws.Cells[rLastRow, 1].value = arab[ComplexItem.rSeconLevelCount++]; // и увеличиваем переменную на 1
+            }
+            if (LSimpleItem.Count > 0)// если в статье сразу есть пункты расходов - то выводим их (!Важно - даже если там есть еще комплексы - их вывод игнорится)
+            {
+                if (LSimpleItem.Count == 1 && LSimpleItem[0].Name==this.Name) //если всего один пункт расходов и его имя совпадает с названием статьи - его не выводим
+                {
+                    
+                    ws.Cells[rLastRow, 3].value = LSimpleItem[0].ValuePeriod;
+                    ws.Cells[rLastRow, 4].value = LSimpleItem[0].ValueYear;
+                    rItog = rLastRow++;
+                    rLastRow++;
+                }
+                else//если пунктов больше
+                {
+                    rLastRow++;
+                    long r1;
+                    long r2;
+                    long k = 1;
+                    r1 = rLastRow;
+                    foreach (SimpleItem si in LSimpleItem) //каждый симпл выводим
+                    {
+                        si.PrintData ( ws, ref rLastRow, k++ );
+                    }
+                    r2 = rLastRow;
+                    r2--;
+                    ws.Cells[rLastRow, 2].value = "Итого по группе: " + Name; //в конце пишем итог
+                    //  ws.Range[ws.Cells[rLastRow, 2], ws.Cells[rLastRow, 4]].interior.colorindex = _colorIndex;
+                    ws.Range[ws.Cells[rLastRow, 2], ws.Cells[rLastRow, 4]].font.bold = true;
+                    //ws.Cells[rLastRow++, 3].value = r1 + "-" + r2;
+                    rItog = rLastRow;
+                    ws.Cells[rLastRow, 3].formulaR1C1 = "=SUM(R" + r1 + "C:R" + r2 + "C)"; //и формулы итогов для периода и года
+                    ws.Cells[rLastRow++, 4].formulaR1C1 = "=SUM(R" + r1 + "C:R" + r2 + "C)";
+                    rLastRow++;
+                }
+            }
+            else//  если нету симплов)
+            {
+
+                if (LComplexItem.Count > 0) // и есть комплексы
+                {
+                    rLastRow++;
+                    List<string> rList = new List<string> ();
+
+                    foreach (ComplexItem c in LComplexItem) //для каждого комплекса
+                    {
+                        //if (Name == "Имущество,Капит. влож. Инвестиции")
+                        //    testdel = 5;
+
+                        long rItogComplex = c .PrintData ( ws ); //комплекс выводится на лист (рекурсия, ага) 
+                        rList.Add ( "R" + rItogComplex + "C" );//и возвращается итоговая строка этого комлекса
+                        //if (Name == "Имущество,Капит. влож. Инвестиции")
+                        //    testdel = 5;
+                    }
+
+                    //   string f2 = "=";
+                    //for (int i = 0; i < rList.Count; i++)
+                    //{
+                    //    f1 = f1 +"+" + rList[i];
+
+                    //}
+
+                    string f1 = "0";
+                    if (rList.Count > 0) //когда все комплексы принадлежащие этому комплексу пройдены - подбивается строка итога вида =r15c3+r25c3...
+                        f1 = "=" + string.Join ( "+", rList );
+                    rItog = rLastRow;
+                    ws.Cells[rLastRow, 2].value = "Итого по группе: " + Name;
+                  //  ws.Range[ws.Cells[rLastRow, 2], ws.Cells[rLastRow, 4]].interior.colorindex = _colorIndex;
+                    ws.Range[ws.Cells[rLastRow, 2], ws.Cells[rLastRow, 4]].font.bold = true;
+                    ws.Cells[rLastRow, 3].formular1c1 = f1;
+                    ws.Cells[rLastRow++, 4].formular1c1 = f1;
+                    rLastRow++;
+                }
+                else //и если нет не комплексов не симплов
+                {
+                    rItog = rLastRow;
+                    ws.Cells[rLastRow, 3].value = 0;
+                    ws.Cells[rLastRow++, 4].value = 0;
+                    
+                    rLastRow++;
+                }
+            }
+            //if (rItog == 777)
+            //{
+            //    long k = 1;
+            //}
+            return rItog;
+        }
+        public override string ToString() {
+            return _name;
+        }
+    }
+
+    
+}
